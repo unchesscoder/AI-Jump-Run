5d0c08740c61b9e32e9612fa6f0792bea04718d1
unchesscoder <DaxQuin7@gmx.de>
2024-02-02 01:19:45 +0100

    Reset to base game

--- ground.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Ground:
-    def __init__(self, height, width):
-        self.height = height
-        self.width = width
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, (255, 255, 255), [0, screen.get_height() - self.height, self.width, self.height])
